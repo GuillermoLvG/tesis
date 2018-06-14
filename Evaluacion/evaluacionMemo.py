f658a2f678810e6f6ae62380212ce348b4a5a005
Guillermo López Velarde González <guillermo.lvg@gmail.com>
2018-06-14 13:35:30 -0500

    evaluación terminada

--- Evaluacion/evaluacionMemo.py
+++ Evaluacion/evaluacionMemo.py
@@ -10,13 +10,22 @@ import openpyxl
 import xml.etree.ElementTree
 from pymongo import MongoClient
 import csv
+try:
+	os.remove("tabla.csv")
+	os.remove("tablaFL.csv")
+	os.remove("tablaMemo.csv")
+except OSError:
+	pass
 
 #Gold Standard
-doc = openpyxl.load_workbook('entities_2018.xlsx')
+doc = openpyxl.load_workbook('entities_ajeno.xlsx')
 sheet = doc['Sheet1']
 entidades = []
-for reng in range(1,60):
-	entidades.append([sheet.cell(row=reng,column=1).value,sheet.cell(row=reng,column=2).value])
+row_count = sheet.max_row + 1
+#desde empieza hasta 1 más
+for reng in range(2,row_count):
+	print (sheet.cell(row=reng,column=3).value)
+	entidades.append([sheet.cell(row=reng,column=3).value,sheet.cell(row=reng,column=4).value])
 entidades = [list(item) for item in set(tuple(row) for row in entidades)]
 for entidad in entidades:
 	escribir = csv.writer(open('tabla.csv', 'a+'))
@@ -24,7 +33,16 @@ for entidad in entidades:
 
 goldStandard = []
 for entidad in entidades:
-	goldStandard.append(entidad[0].lower())
+	valor = entidad[0]
+	valor = valor.strip()
+	valor = valor.replace(",","")
+	valor = valor.replace(")","")
+	valor = valor.replace("(","")
+	valor = valor.replace("”","")
+	valor = valor.replace("“","")
+	valor = valor.replace(". ",".")
+	goldStandard.append(valor.lower())
+print (goldStandard)
 
 #Resultado Freeling
 client = MongoClient('localhost', 27017)
@@ -33,7 +51,13 @@ collection = db.collection
 result = collection.find({})
 freelingResults = []
 for element in result:
-	freelingResults.append(element["Nombre"].lower())
+	valor = element["Nombre"]
+	valor = valor.replace(" de el "," del ")
+	valor = valor.replace(",","")
+	valor = valor.replace(")","")
+	valor = valor.replace("(","")
+	valor = valor.replace(". ",".")
+	freelingResults.append(valor.lower())
 	escribir = csv.writer(open('tablaFL.csv', 'a+'))
 	escribir.writerow([element["Nombre"],element["Clase"]])
 #Calculo de precisión y exhaustividad
@@ -63,7 +87,10 @@ collection = db.Entidades
 result = collection.find({})
 memoResults = []
 for element in result:
-	memoResults.append(element["Nombre"].lower())
+	valor = element["Nombre"]
+	valor = valor.replace(" de el "," del ")
+	valor = valor.replace(". ",".")
+	memoResults.append(valor.lower())
 	escribir = csv.writer(open('tablaMemo.csv', 'a+'))
 	escribir.writerow([element["Nombre"],element["Clase"]])
 
@@ -72,14 +99,17 @@ TP = 0
 FP = 0
 for element in memoResults:
 	if element in goldStandard:
+		#print ("Verdadero Positivo:" + element)
 		TP = TP + 1
 	else:
 		FP = FP + 1
+		print("Falso Positivo:" + element)
 precision = (TP/(TP+FP))*100
 FN = 0
 for element in goldStandard:
 	if element not in memoResults:
 		FN = FN + 1
+		print("Falso Negativo:" + element)
 recall = (TP/(TP+FN))*100
 
 print("Memo")
